b13a0520000c710546839db71a9cbee7f4dffd77
Amhjadkhan <amjadpogramer@gmail.com>
2024-09-03 17:47:58 +0400

    this is header section added commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./component/Header/Header";
 import Navber from "./component/Navber/Navber";
 
 function App() {
   return (
     <>
       <Navber />
+      <Header />
     </>
   );
 }
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,30 @@
+
+const Header = () => {
+  return (
+    <header className="bg-gradient-to-r from-purple-500 to-[#0A1830] text-white p-10">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold pb-4">Multi-Blockchain DeFi Earn And <br /> Borrow Across Networks</h1>
+        <p className="mt-4 text-xl pb-3">
+          Whether it's earning interest on their digital assets or taking out loans, individuals can now explore <br /> opportunities beyond the limitations of a single blockchain.
+        </p>
+        <button className="btn mt-6 p-5 rounded-md bg-blue-300 text-black text-xl font-semibold">Launch App</button>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 mt-10">
+        <div className="bg-gray-800 p-6 rounded border-2">
+          <p className="text-center text-2xl font-semibold">Total Market Size</p>
+          <h4 className="text-center text-2xl font-semibold">$605.04M</h4>
+        </div>
+        <div className="bg-gray-800 p-6 rounded border-2">
+          <p className="text-center text-2xl font-semibold">Platform fees paid to RDNT lockers</p>
+          <h4 className="text-center text-2xl font-semibold">$11.91M</h4>
+        </div>
+        <div className="bg-gray-800 p-6 rounded border-2">
+          <p className="text-center text-2xl font-semibold">Looping APR on BNB</p>
+          <h4 className="text-center text-2xl font-semibold">~0%</h4>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
