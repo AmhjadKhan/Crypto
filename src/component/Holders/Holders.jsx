cb132cd81d1696e013fea6d7f7ab8dd67db52e40
Amhjadkhan <amjadpogramer@gmail.com>
2024-09-04 03:01:52 +0400

    this is headers section done commit

--- src/component/Holders/Holders.jsx
+++ src/component/Holders/Holders.jsx
@@ -10,20 +10,20 @@ const Holders = () => {
         <p className="mt-4 text-white text-xl">Battle-tested and audited by multiple leading security firms. Radiant's <br /> security is of the highest priority.</p>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 mt-10">
           <div className='border-r-2'>
-            <img src={security} alt="holder" className="mx-auto"/>
-            <h4 className="mt-4 text-xl">Better Security</h4>
+            <img src={security} alt="holder" className="mx-auto bg-purple-500 p-10 rounded-full"/>
+            <h4 className="mt-4 text-2xl text-black font-bold">Better Security</h4>
           </div>
           <div className='border-r-2'>
-            <img src={blockchain} alt="holder" className="mx-auto"/>
-            <h4 className="mt-4 text-xl">BlockSec</h4>
+            <img src={blockchain} alt="holder" className="mx-auto bg-purple-500 p-10 rounded-full"/>
+            <h4 className="mt-4 text-2xl text-black font-bold">BlockSec</h4>
           </div>
           <div>
-            <img src={time} alt="holder" className="mx-auto"/>
-            <h4 className="mt-4 text-xl">Better Uptime</h4>
+            <img src={time} alt="holder" className="mx-auto bg-[#0A1830] p-10 rounded-full"/>
+            <h4 className="mt-4 text-2xl text-black font-bold">Better Uptime</h4>
           </div>
         </div>
-        <div className="mt-8">
-          BlockSec focuses on the security of the whole life cycle of smart contracts, specializing in rigorous testing of economic attacks.
+        <div className="mt-8 text-xl font-medium text-white border-2 p-4 ml-32 mr-32 bg-slate-500">
+          BlockSec focuses on the security of the whole life cycle of smart contracts,<br /> specializing in rigorous testing of economic attacks.
         </div>
       </div>
     </section>
